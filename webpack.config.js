24aa47f2607d6b641ac633f1e01789ece75538f4
Antonio Pierro <antonio.pierro@gmail.com>
2020-10-12 14:33:03 +0200

    adding control flow

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
                         options: {
                             data: {
                                 name: "Antonio",
+                                dev_evn: true,
                             },
                         },
                     },
